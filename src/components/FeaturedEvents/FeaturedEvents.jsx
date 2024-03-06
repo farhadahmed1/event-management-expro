5250b3e9ce5001c526a89fab6b2eb1cc5ecea181
farhadahmed1 <farhad.ahmed.aust@gamil.com>
2024-03-06 17:30:15 +0600

    addede feature event

--- src/components/FeaturedEvents/FeaturedEvents.jsx
+++ src/components/FeaturedEvents/FeaturedEvents.jsx
@@ -1,11 +1,45 @@
-import React from "react";
-
-const FeaturedEvents = () => {
+import PropTypes from "prop-types";
+import { FaLocationDot } from "react-icons/fa6";
+const FeaturedEvents = ({ event }) => {
+  console.log(event);
+  const { title, id, data, eventLocation, image, details } = event;
   return (
     <div>
-      <h2> Featured Event is coming soon ...</h2>
+      {/* <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img src={image} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p>{details}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div> */}
+
+      <div className="card w-96 glass">
+        <figure>
+          <img src={image} alt="car!" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p>{details}</p>
+          <p className="justify-center flax">
+            <FaLocationDot />
+            {eventLocation}
+          </p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Learn now!</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default FeaturedEvents;
+
+FeaturedEvents.propTypes = {
+  event: PropTypes.array.isRequired,
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,36 @@
+import { Link, useLoaderData } from "react-router-dom";
 import Banner from "../../components/Banner/Banner";
+import FeaturedEvents from "../../components/FeaturedEvents/FeaturedEvents";
 
 const Home = () => {
-  return <Banner></Banner>;
+  const featuredEvent = useLoaderData();
+  //console.log(featuredEvent);
+  return (
+    <>
+      <Banner></Banner>
+      <div>
+        <div className="text-center">
+          <h2 className="text-4xl mt-3 font-bold"> Featured Events</h2>
+          <p className="text-xl mt-3"> You can configure featured in backend</p>
+          <hr className="w-40 h-0.5 mx-auto mb-10 mt-1 bg-red-500 border-0 rounded  dark:bg-gray-700"></hr>
+        </div>
+        <div className="grid grid-cols-2 m-4">
+          {featuredEvent.map((event) =>
+            event.length > 4 ? (
+              <>
+                <FeaturedEvents key={event.id} event={event} />
+                <Link to="/moreEvents">More Event</Link>
+              </>
+            ) : (
+              <>
+                <FeaturedEvents key={event.id} event={event} />
+              </>
+            )
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
